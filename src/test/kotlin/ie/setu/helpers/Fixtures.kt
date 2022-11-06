c9f10742dfe748cabc37a3398add5c4cff09d77b
kiran <20100681@mail.wit.ie>
2022-11-06 15:45:24 +0000

    update Exercise tracker feature

--- src/test/kotlin/ie/setu/helpers/Fixtures.kt
+++ src/test/kotlin/ie/setu/helpers/Fixtures.kt
@@ -1,9 +1,7 @@
 package ie.setu.helpers
 
 import ie.setu.domain.*
-import ie.setu.domain.db.CaloriesTracker
 import ie.setu.domain.db.Users
-import ie.setu.domain.repository.CaloriesTrackerDAO
 import ie.setu.domain.repository.UserDAO
 import ie.setu.repository.user1
 import ie.setu.repository.user2
@@ -22,11 +20,6 @@ val users = arrayListOf<User>(
     User(name = "Carol Singer", email = "carol@singer.com", id = 4)
 )
 
-val caloriestrackers = arrayListOf<CaloriesTrackerDC>(
-    CaloriesTrackerDC(1,1, DateTime.now(),"Walking",10,40.0),
-    CaloriesTrackerDC(2,2, DateTime.now(),"Jogging",8,106.64),
-    CaloriesTrackerDC(3,3, DateTime.now(),"Cycling",15,99.9)
-)
 
 val exercisetrackers = arrayListOf<ExerciseTrackerDC>(
     ExerciseTrackerDC(1,"Monday","Arms",10),
