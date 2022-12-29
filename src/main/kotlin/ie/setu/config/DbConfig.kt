1e352c6907f4b20895224c2b2d94b1012274ea31
kiran <20100681@mail.wit.ie>
2022-12-29 00:53:35 +0000

    Add dbconfig changes

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -14,7 +14,7 @@ class DbConfig{
         logger.info{"Starting DB Connection..."}
 
         val dbConfig = Database.connect(
-
+//heroku details
 //            "jdbc:postgresql://ec2-52-207-15-147.compute-1.amazonaws.com:5432/d8audr61018bo9?sslmode=require",
 //            driver = "org.postgresql.Driver",
 //            user = "cpyakngkskdiyl",
